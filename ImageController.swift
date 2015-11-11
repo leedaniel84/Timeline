2ac7754a4ebf33488f946cea2e4fffb7ff8d8852
Daniel Lee <lee.daniel84@gmail.com>
2015-11-11 15:12:41 -0700

    ImageController Firebase completed

--- ImageController.swift
+++ ImageController.swift
@@ -13,12 +13,50 @@ class ImageController {
     
     static func uploadImage(image: UIImage, completion: (identifier: String?) -> Void) {
         
-        
+        if let base64Image = image.base64String {
+            let base = FirebaseController.base.childByAppendingPath("images").childByAutoId()
+            base.setValue(base64Image)
+            
+            completion(identifier: base.key)
+        } else {
+            completion(identifier: nil)
+        }
         
     }
     
     static func imageForIdentifier(identifier: String, completion: (image: UIImage?) -> Void) {
         
+        FirebaseController.dataAtEndpoint("images/\(identifier)") { (data) -> Void in
+            
+            if let data = data as? String {
+                let image = UIImage(base64: data)
+                completion(image: image)
+            } else {
+                completion(image: nil)
+            }
+        }
+        
+    }
+    
+}
+
+extension UIImage {
+    
+    var base64String: String? {
+        guard let data = UIImageJPEGRepresentation(self, 0.0) else {
+            return nil
+        }
+        
+        return data.base64EncodedStringWithOptions(.EncodingEndLineWithCarriageReturn)
+        
+    }
+    
+    convenience init?(base64: String) {
+        if let imageData = NSData(base64EncodedString: base64, options: .IgnoreUnknownCharacters) {
+            self.init(data: imageData)
+        } else {
+            return nil
+        }
     }
     
 }
